6c6006418fd937975778ba1215b5a32c9e2897bd
张博 <zbjumper@gmail.com>
2021-12-02 20:06:56 +0800

    格式化

--- packages/tibox/src/config.ts
+++ packages/tibox/src/config.ts
@@ -22,7 +22,7 @@ import { parseDestFolderName, parseProjectName } from "./libs/tools";
 import loadJsonFile from "load-json-file";
 import { LogLevel } from "./logger";
 import _ from "lodash";
-import git from "git-rev-sync"
+import git from "git-rev-sync";
 
 // import { CLIENT_DIR, DEFAULT_ASSETS_RE } from './constants'
 export interface ConfigEnv {
